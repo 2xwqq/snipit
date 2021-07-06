3876a1bbed8301497dc9dfc16212fcf3b0861cc3
Alex Dementsov <dexity@gmail.com>
2021-07-05 21:50:47 -0700

    Изменили структуру кода для server и client директорий

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from flask import render_template
-from app import app
+from flask import Flask
 
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     posts = [
