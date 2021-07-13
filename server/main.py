4cce80e83e4e0239bdc3634df79bc5604e7935a7
Alex Dementsov <dexity@gmail.com>
2021-07-12 22:20:08 -0700

    Написал функцию shot() и добавил примеры скриншотов. Представил пример использования случайной функции.

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
+import random
 import json
 import os
 import flask
+from flask import abort
 from flask import render_template
 from flask import jsonify
 from flask import request
@@ -8,10 +10,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+DB = 'db.txt'
+
 # Web application
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/<path:id>')
+def shot(id):
+    name = image_by_id(id)
+    if not name:
+        abort(404)
+    return flask.send_from_directory('uploaded', name)
 
 
 @app.route('/images/<path:name>')
@@ -19,11 +31,25 @@ def images(name):
     return flask.send_from_directory('templates/images', name)
 
 
+def image_by_id(id):
+    """Find filename from id."""
+    with open(DB) as f:
+        for line in f:
+            line = line.rstrip()
+            row = line.split(',')
+            if row[0] == id:
+                return row[1]
+    return None
+
+
 # API
 # Test
 @app.route('/v1/test', methods=['POST'])
 def test():
-    d = {'hello': request.json['key']}
+    number = random.randrange(99)
+    with open('db.txt', 'a') as f:
+        f.write(f'\n{number}')
+    d = {'hello': number}
     return jsonify(d)
 
 
@@ -33,4 +59,12 @@ def upload():
     filename = file.filename
     file.save(f'{os.getcwd()}/uploaded/{filename}')
     d = {'file': filename}
+    # TODO: Save file in "uploaded" directory
+    # >TODO: Get random number: 34
+    # TODO: Convert current time to timestamp
+    # >TODO: Add line to db.txt: open(filename, 'a')
     return jsonify(d)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
